e9794c22f285125bf616c82a870a2404b7262d74
Yudi Alfayat <59765710+ylfyt@users.noreply.github.com>
2022-02-06 17:12:50 +0700

    Create credential.h

---
+++ include/credential.h
@@ -0,0 +1,9 @@
+#ifndef __CREDENTIAL_H__
+
+#define WIFI_SSID "ssid"
+#define WIFI_PASSWORD "pass"
+#define API_PASSWORD "pass"
+#define OTA_USERNAME "username"
+#define OTA_PASSWORD "pass"
+
+#endif
