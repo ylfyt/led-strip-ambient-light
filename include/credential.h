2fc82a2c609a37035fc9ba8e5594a5a16e6a2abd
yudialfayat <ayudi3521@gmail.com>
2022-02-06 17:09:34 +0700

    Untrack credential.h

--- include/credential.h
+++
@@ -1,9 +0,0 @@
-#ifndef __CREDENTIAL_H__
-
-#define WIFI_SSID "ssid"
-#define WIFI_PASSWORD "pass"
-#define API_PASSWORD "pass"
-#define OTA_USERNAME "username"
-#define OTA_PASSWORD "pass"
-
-#endif
